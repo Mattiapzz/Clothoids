79a682e51a4909bacdefcecf7faf03907beefa13
DESKTOP-SC4PU2T\carmaker <mattia.piazza@unitn.it>
2024-03-07 17:46:16 +0100

    fix for clothoids in mingw

--- src/Clothoids.hh
+++ src/Clothoids.hh
@@ -31,7 +31,7 @@
 #define CLOTHOIDS_dot_HH
 
 // comment to disable threads support
-#define CLOTHOIDS_USE_THREADS 1
+// #define CLOTHOIDS_USE_THREADS 1
 
 #include "Utils.hh"
 #include "Utils_AABB_tree.hh"
